3b83cb18c3fd3f1887f15b0e6595c619bc0e2f34
Shreyash <chamaria.21.shreyash@gmail.com>
2020-05-10 16:01:13 +0530

    SCM_BLOCKCHAIN

--- contracts/ChangeOwnership.sol
+++ contracts/ChangeOwnership.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.23;
 
+
 contract ProductManagement {
     struct Part {
         address manufacturer;
@@ -14,74 +15,127 @@ contract ProductManagement {
         string serial_number;
         string product_type;
         uint256 creation_date;
-        bytes32[6] parts;
+        bytes32[5] parts;
     }
 
     mapping(bytes32 => Part) public parts;
     mapping(bytes32 => Product) public products;
 
-    function getParts(bytes32) public pure returns (bytes32[6] memory){}
+    function getParts(string serial_number)
+        public
+        view
+        returns (string, string, string, string, string)
+    {}
 }
-contract ChangeOwnership{
-  ProductManagement private pm ;
-  constructor(address pm_addr) public {
-    pm = ProductManagement(pm_addr);
-  }
-  enum OperationType{Part,Product}
 
-  mapping(bytes32 => address) CurrentPartOwner ;
-  mapping(bytes32 => address) CurrentProductOwner ;
 
-  event TransferPartOwnership(bytes32 indexed p, address indexed account);
+contract ChangeOwnership {
+    ProductManagement private pm;
+
+    constructor(address pm_addr) public {
+        pm = ProductManagement(pm_addr);
+    }
+
+    enum OperationType {Part, Product}
+
+    mapping(bytes32 => address) CurrentPartOwner;
+    mapping(bytes32 => address) CurrentProductOwner;
+
+    event TransferPartOwnership(bytes32 indexed p, address indexed account);
     event TransferProductOwnership(bytes32 indexed p, address indexed account);
 
-  function addOwnership(uint op_type, bytes32 p_hash) public returns (bool) {
-    address manufacturer;
+    function getPartList(string serial_number)
+        private
+        view
+        returns (bytes32[5])
+    {
+        string memory p1;
+        string memory p2;
+        string memory p3;
+        string memory p4;
+        string memory p5;
+        (p1, p2, p3, p4, p5) = pm.getParts(serial_number);
+        bytes32[5] memory part_list;
+        part_list[0] = keccak256(p1);
+        part_list[1] = keccak256(p2);
+        part_list[2] = keccak256(p3);
+        part_list[3] = keccak256(p4);
+        part_list[4] = keccak256(p5);
+        return part_list;
+    }
 
-        if(op_type == uint(OperationType.Part)){
-            
-            (manufacturer, , , ,) = pm.parts(p_hash);
-            require(CurrentPartOwner[p_hash] == address(0), "Part was already registered");
-            require(manufacturer == msg.sender, "Part was not made by requester");
+    function addOwnership(uint256 op_type, string serial_number) public {
+        address manufacturer;
+        bytes32 p_hash = keccak256(serial_number);
+
+        if (op_type == uint256(OperationType.Part)) {
+            (manufacturer, , , , ) = pm.parts(p_hash);
+            require(
+                CurrentPartOwner[p_hash] == address(0),
+                "Part was already registered"
+            );
+            require(
+                manufacturer == msg.sender,
+                "Part was not made by requester"
+            );
             CurrentPartOwner[p_hash] = msg.sender;
             emit TransferPartOwnership(p_hash, msg.sender);
-            return true;
-        } else if (op_type == uint(OperationType.Product)){
-            
+        } else if (op_type == uint256(OperationType.Product)) {
             (manufacturer, , , , ) = pm.products(p_hash);
-            require(CurrentProductOwner[p_hash] == address(0), "Product was already registered");
-            require(manufacturer == msg.sender, "Product was not made by requester");
-            bytes32[6] memory part_list = pm.getParts(p_hash);
-            for(uint j=0;j<part_list.length;j++)
-            {
-                require(CurrentPartOwner[part_list[j]]==msg.sender,"Part not owned by requester");
+            require(
+                CurrentProductOwner[p_hash] == address(0),
+                "Product was already registered"
+            );
+            require(
+                manufacturer == msg.sender,
+                "Product was not made by requester"
+            );
+            bytes32[5] memory part_list = getPartList(serial_number);
+            for (uint256 j = 0; j < part_list.length; j++) {
+                require(
+                    CurrentPartOwner[part_list[j]] == msg.sender,
+                    "Part not owned by requester"
+                );
             }
             CurrentProductOwner[p_hash] = msg.sender;
             emit TransferProductOwnership(p_hash, msg.sender);
-            return true;
         }
     }
 
-
-    function changeOwnership(uint op_type, bytes32 p_hash, address to) public returns (bool) {
-      //Check if the element exists and belongs to the user requesting ownership change
-        if(op_type == uint(OperationType.Part)){
-            require(CurrentPartOwner[p_hash] == msg.sender, "Part is not owned by requester");
+    function changeOwnership(uint256 op_type, string serial_number, address to)
+        public
+    {
+        bytes32 p_hash = keccak256(serial_number);
+        //Check if the element exists and belongs to the user requesting ownership change
+        if (op_type == uint256(OperationType.Part)) {
+            require(
+                CurrentPartOwner[p_hash] == msg.sender,
+                "Part is not owned by requester"
+            );
             CurrentPartOwner[p_hash] = to;
             emit TransferPartOwnership(p_hash, to);
-            return true;
-        } else if (op_type == uint(OperationType.Product)){
-            require(CurrentProductOwner[p_hash] == msg.sender, "Product is not owned by requester");
+        } else if (op_type == uint256(OperationType.Product)) {
+            require(
+                CurrentProductOwner[p_hash] == msg.sender,
+                "Product is not owned by requester"
+            );
             CurrentProductOwner[p_hash] = to;
             emit TransferProductOwnership(p_hash, to);
             //Change part ownership too
-            bytes32[6] memory part_list = pm.getParts(p_hash);
-            for(uint i = 0; i < part_list.length; i++){
+            bytes32[5] memory part_list = getPartList(serial_number);
+            for (uint256 i = 0; i < part_list.length; i++) {
                 CurrentPartOwner[part_list[i]] = to;
                 emit TransferPartOwnership(part_list[i], to);
-                return true;
             }
-
+        }
+    }
+    function currentOwner (uint256 op_type,string serial_number) public view returns (address){
+        bytes32 p_hash = keccak256(serial_number);
+        if(op_type == uint256(OperationType.Part)){
+            return CurrentPartOwner[p_hash];
+        }
+        else if(op_type == uint256(OperationType.Product)){
+            return CurrentProductOwner[p_hash];
         }
     }
 }
--- contracts/ProductManagement.sol
+++ contracts/ProductManagement.sol
@@ -15,7 +15,7 @@ contract ProductManagement {
         string serial_number;
         string product_type;
         uint256 creation_date;
-        bytes32[6] parts;
+        bytes32[5] parts;
     }
 
     mapping(bytes32 => Part) public parts;
@@ -23,9 +23,7 @@ contract ProductManagement {
 
     constructor() public {}
 
-    function buildPart(string memory serial_number, string memory part_type)
-        public
-        returns (bytes32)
+    function buildPart(string memory serial_number, string memory part_type)public
     {
         bytes32 part_hash = keccak256(serial_number);
 
@@ -34,8 +32,6 @@ contract ProductManagement {
         require(
             parts[part_hash].manufacturer == address(0), "Part already exists.."
         );
-        
-
         Part memory new_part = Part(
             msg.sender,
             serial_number,
@@ -44,14 +40,17 @@ contract ProductManagement {
             false
         );
         parts[part_hash] = new_part;
-        return part_hash;
     }
 
     function buildProduct(
         string memory serial_number,
         string memory product_type,
-        bytes32[6] memory parts_arr
-    ) public returns (bytes32) {
+        string memory part1,
+        string memory part2,
+        string memory part3,
+        string memory part4,
+        string memory part5
+    ) public  {
         bytes32 product_hash = keccak256(serial_number);
 
         uint256 creation_date = now;
@@ -59,17 +58,15 @@ contract ProductManagement {
         require(
             products[product_hash].manufacturer == address(0),"Product already exists.."
         );
-        
-        for(uint i=0;i<parts_arr.length;i++){
+        bytes32[5] memory parts_arr = [keccak256(part1),keccak256(part2),keccak256(part3),keccak256(part4),keccak256(part5)];
+        for(uint i = 0;i<parts_arr.length;i++){
             require(
             parts[parts_arr[i]].manufacturer != address(0),"Part does not exists.."
         );
         require(parts[parts_arr[i]].used != true, "Part already used");
         }
-        
-        for(uint j=0;j<parts_arr.length;j++){
+        for(uint j = 0;j<parts_arr.length;j++){
             parts[parts_arr[j]].used = true;
-        
         }
         Product memory new_product = Product(
             msg.sender,
@@ -79,12 +76,22 @@ contract ProductManagement {
             parts_arr
         );
         products[product_hash] = new_product;
-        return product_hash;
     }
+function showPart(string serial_number) public view returns(address,string,string,uint256,bool)
+{
+    bytes32 part_hash = keccak256(serial_number);
+    return (parts[part_hash].manufacturer,parts[part_hash].serial_number,parts[part_hash].part_type,parts[part_hash].creation_date,parts[part_hash].used);
+}
 
-    function getParts(bytes32 product_hash) public view returns (bytes32[6] memory){
+function showProduct(string serial_number) public view returns(address,string,string,uint256)
+{
+    bytes32 product_hash = keccak256(serial_number);
+    return (products[product_hash].manufacturer,products[product_hash].serial_number,products[product_hash].product_type,products[product_hash].creation_date);
+}
+    function getParts(string serial_number) public view returns (string,string,string,string,string){
+        bytes32 product_hash = keccak256(serial_number);
         //The automatic getter does not return arrays, so lets create a function for that
         require(products[product_hash].manufacturer != address(0));
-        return products[product_hash].parts;
+        return(parts[products[product_hash].parts[0]].serial_number,parts[products[product_hash].parts[1]].serial_number,parts[products[product_hash].parts[2]].serial_number,parts[products[product_hash].parts[3]].serial_number,parts[products[product_hash].parts[4]].serial_number);
     }
 }
